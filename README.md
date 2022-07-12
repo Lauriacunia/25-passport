6081aed104bc8ea135258c82285e89500a92b5bf
Lauriacunia <lauriacunia@gmail.com>
2022-07-12 11:56:28 -0300

    Creo vistas y routeo

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# 25-passport
+### 💓👍💓 Si te fue útil éste repositorio dejame lindas valoraciones en la clase 💓👍💓
+
+```
+nmp start
+```
+
+### dependencias
+
+```
+npm i express
+npm i express-session
+npm i session-file-store --save
+npm i cookie-parser --save
+npm i ejs
+npm i bulma
+npm i dotenv
+npm i mongoose
+npm i connect-mongo
+npm i passport passport-local
+```
---
+++ server.js
@@ -0,0 +1,35 @@
+import express  from "express";
+import session from "express-session";
+import sessionFileStore from "session-file-store";
+import apiRoutes from "./src/routes/apiRoutes";
+const fileStore = sessionFileStore(session);
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(
+    {
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+        store: new fileStore({
+            path: './src/sesiones',
+            ttl: 60
+        })
+    }
+));
+
+app.set('views', 'src/views');
+app.set('view engine', 'ejs');
+
+
+/** Routes */
+app.use('/', apiRoutes);
+
+
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    }
+);
